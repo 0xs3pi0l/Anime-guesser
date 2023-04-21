4572cf7e4255c2c53d22cc2bb054dce4e995e247
0xs3pi0l <omarpasculli2001@gmail.com>
2023-04-21 17:01:04 +0200

    Add function to insert quote

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 let playButton = document.getElementsByTagName("button");
 
+function insertQuote(quote){
+    let h1 = document.getElementsByTagName("h1");
+    h1[0].style.fontSize = "50px";
+    h1[0].style.textAlign = "match-parent";
+    h1[0].innerHTML = quote;
+}
+
 function fetchQuote(){
     fetch('https://animechan.vercel.app/api/random')
         .then(response => response.json())
+        .then(quote => insertQuote(quote.quote));
+    
 }
 
 playButton[0].addEventListener("click", fetchQuote);
