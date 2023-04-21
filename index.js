187127c3174a284e5070d41f899b5bb0c90f02bd
0xs3pi0l <omarpasculli2001@gmail.com>
2023-04-21 17:49:50 +0200

    Add function to swap buttons

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 let playButton = document.getElementsByTagName("button");
 
-function insertQuote(quote){
+function swapButton(animeTitle){
+    playButton[0].remove();
+    let button1 = document.createElement("button");
+    let button2 = document.createElement("button");
+    let button3 = document.createElement("button");
+    button1.innerHTML = button2.innerHTML = button3.innerHTML = animeTitle;
+    let div = document.getElementsByTagName("div");
+    div[0].appendChild(button1);
+    div[0].appendChild(button2);
+    div[0].appendChild(button3);
+}
+
+function swapContent(quote){
     let h1 = document.getElementsByTagName("h1");
     h1[0].style.fontSize = "50px";
     h1[0].style.textAlign = "match-parent";
@@ -10,8 +22,10 @@ function insertQuote(quote){
 function fetchQuote(){
     fetch('https://animechan.vercel.app/api/random')
         .then(response => response.json())
-        .then(quote => insertQuote(quote.quote));
+        .then(quote => swapContent(quote.quote));
     
 }
+
+
 
 playButton[0].addEventListener("click", fetchQuote);
