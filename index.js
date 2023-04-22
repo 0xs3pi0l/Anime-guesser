0b2de949d6d99bcc2a3828e41662b397355e8746
0xs3pi0l <omarpasculli2001@gmail.com>
2023-04-22 15:19:51 +0200

    Add fetching side of the project

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
 let playButton = document.getElementsByTagName("button");
 
-function swapButton(animeTitle){
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function fetchTitles(rightTitle){
+    fetch('https://animechan.vercel.app/api/available/anime')
+      .then(response => response.json())
+      .then(animes => {
+        let randomIndex = getRandomInt(60);
+        let wrongTitle1 = animes[randomIndex];
+        randomIndex = getRandomInt(60);
+        let wrongTitle2 = animes[randomIndex];
+        let titles = new Array(3);
+        let randomStartingIndex = getRandomInt(3);
+        switch(true){
+            case (randomStartingIndex == 0):
+                titles[0] = rightTitle;
+                titles[1] = wrongTitle1;
+                titles[2] = wrongTitle2;
+                break;
+            case (randomStartingIndex == 1):
+                titles[0] = wrongTitle1;
+                titles[1] = rightTitle;
+                titles[2] = wrongTitle2;
+                break;
+            case (randomStartingIndex == 2):
+                titles[0] = wrongTitle1;
+                titles[1] = wrongTitle2;
+                titles[2] = rightTitle;
+                break;
+        }
+        swapButton(titles);
+      })
+}
+
+function swapButton(titles){
     playButton[0].remove();
     let button1 = document.createElement("button");
     let button2 = document.createElement("button");
     let button3 = document.createElement("button");
-    button1.innerHTML = button2.innerHTML = button3.innerHTML = animeTitle;
     let div = document.getElementsByTagName("div");
+    button1.innerHTML = titles[0];
+    button2.innerHTML = titles[1];
+    button3.innerHTML = titles[2];
     div[0].appendChild(button1);
     div[0].appendChild(button2);
     div[0].appendChild(button3);
 }
 
 function swapContent(quote){
+    //Swap anime title
     let h1 = document.getElementsByTagName("h1");
     h1[0].style.fontSize = "50px";
     h1[0].style.textAlign = "match-parent";
     h1[0].innerHTML = quote.quote;
-    swapButton(quote.anime);
+
+    fetchTitles(quote.anime);
 }
 
 function fetchQuote(){
     fetch('https://animechan.vercel.app/api/random')
         .then(response => response.json())
         .then(quote => swapContent(quote));
-    
 }
-
-
 
 playButton[0].addEventListener("click", fetchQuote);
